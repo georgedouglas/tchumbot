7fd65d8d852bd8dad2f40348080d646a278eb84c
georgedouglas <48889536+georgedouglas@users.noreply.github.com>
2019-07-20 21:01:08 -0300

    Update: Everything in English now

--- bot.js
+++ bot.js
@@ -1,39 +1,25 @@
 
-// Extract the required classes from the discord.js module
 const { Client, RichEmbed } = require('discord.js');
 
-// Create an instance of a Discord client
 const client = new Client();
 const auth = require('./auth.json');
 
-/**
- * The ready event is vital, it means that only _after_ this will your bot start reacting to information
- * received from Discord
- */
 client.on('ready', () => {
-  console.log(`Logado como ${client.user.tag}!`);
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  // If the message is "how to embed"
-  if (msg.content === 'embede') {
-    // We can create embeds using the MessageEmbed constructor
-    // Read more about all that you can do with the constructor
-    // over at https://discord.js.org/#/docs/main/stable/class/RichEmbed
+  if (msg.content === 'embed') {
     const embed = new RichEmbed()
-      // Set the title of the field
-      .setTitle('Seja Bem Vindo(a)')
-      // Set the color of the embed
+      .setTitle('Embed Test')
       .setColor(0x239B56)
-      // Set the main content of the embed
-      .setDescription('{Nome_d0_usuario}');
-    // Send the embed to the same channel as the message
+      .setDescription('Embed Description');
     msg.channel.send(embed);
   }
-  if (msg.content === 'avat') {
+  if (msg.content === 'avatar') {
   const myavatarEmbed = new RichEmbed()
   .setColor('0xDC143C')
-  .setTitle('Seu Avatar...')
+  .setTitle('Your Avatar...')
   .setImage(msg.author.avatarURL)	
   if (!msg.mentions.users.size) {
   return msg.channel.send(myavatarEmbed);
@@ -41,7 +27,7 @@ client.on('message', msg => {
         var user = msg.mentions.users.first();	
   const avatarEmbed = new RichEmbed()
   .setColor('0xDC143C')
-  .setTitle(`Avatar de ${user.username}...`)
+  .setTitle(`${user.username}'s avatar...`)
   .setImage(user.displayAvatarURL)
   msg.channel.send(avatarEmbed);
   }
@@ -52,7 +38,7 @@ client.on('guildMemberAdd', member => {
   const channel = member.guild.channels.find(ch => ch.name === 'bot-tests');
   // Do nothing if the channel wasn't found on this server
   if (!channel) return;
-channel.send(`Seja bem vindo(a), ${member}  ${member.user.displayAvatarURL}`);
+channel.send(`Welcome to the server, ${member}  ${member.user.displayAvatarURL}`);
 });
 
 client.login(auth.token);
